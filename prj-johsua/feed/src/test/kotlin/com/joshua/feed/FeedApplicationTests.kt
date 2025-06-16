0dd3b65532b80f4bc2db8e5565edbae4db17422a
keunhui.park <keunhui.park@gmail.com>
2025-06-17 01:46:38 +0900

    feed 프로젝트 기본 시작

---
+++ prj-johsua/feed/src/test/kotlin/com/joshua/feed/FeedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.joshua.feed
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FeedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
